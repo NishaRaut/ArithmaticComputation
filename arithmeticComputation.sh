df5c157a641467e0fabd0a2835ff91e4d6caac7b
NishaRaut <nisharaut84@gmail.com>
2020-01-10 12:37:14 +0530

    Added: taking three integers

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,3 +1,23 @@
 #!/bin/bash -x
 
-echo "welcome to arihmetic computation.."
+read -p "Enter first number: " a
+if [ -z "${a//[0-9]}" ] && [ -n "$a" ]
+then
+		read -p "Enter second number: " b
+		if [ -z "${b//[0-9]}" ] && [ -n "$b" ]
+		then
+				read -p "Enter third number: " c
+				if [ -z "${c//[0-9]}" ] && [ -n "$c" ]
+				then
+						echo "ok"
+				else
+						echo "Sorry integers only.."
+				fi
+		else
+				echo "Sorry integers only.."
+		fi
+else
+       echo "Sorry integers only.."
+fi
+#read -p "Enter first number: " b
+#read -p "Enter first number: " c
